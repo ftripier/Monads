6838a29454f798e12f91d6d2cc44725c12375ee9
Felix Tripier <ftripier@gmail.com>
2018-07-22 17:56:18 -0700

    Correct reader monad

---
+++ game_of_life.hs
@@ -0,0 +1,7 @@
+module Main (main, Board) where
+
+import Control.Monad
+
+data Board = Board [[Bool]] deriving (Show)
+
+main = do putStrLn "Hello Felix"
--- monads.js
+++ monads.js
@@ -9,80 +9,32 @@ console.log("regular composition", addEight(5));
 //   (>>=) :: m a -> (a -> m b) -> m b
 //   return :: a -> m a
 
-
 class Reader {
   constructor(f) {
     this.f = f;
   }
 
   bind(b) {
-    return new Reader(compose(b, this.f));
+    const bound = (env) => {
+      const a = this.run(env);
+      const rb = b(a);
+      return rb.run(env);
+    };
+    return new Reader(bound);
   }
 
   static return(a) {
     return new Reader(() => a);
   }
 
-  run() {
-    return this.f();
+  run(e) {
+    return this.f(e);
   }
 }
 
-const monadic = Reader.return(5).bind(addThree).bind(addFive).run();
-console.log("monadic composition", monadic);
+const addEightMonadic = new Reader((env) => env + 3).bind((env) => new Reader((a) => env + a + 5));
+console.log("monadic composition", addEightMonadic.run(5));
 
-class List {
-  constructor(list) {
-    this.list = list;
-  }
-
-  bind(b) {
-    // join . fmap
-    return new List(this.list.map((a) => b(a)).reduce((acc, curr) => (acc.concat(curr)), []));
-  }
-
-  static return(a) {
-    return new List([a]);
-  }
-
-  unwrap() {
-    return this.list;
-  }
-}
-
-const monadicList = new List([1, 2, 3]).bind((a) => ([addThree(a)]));
-console.log(monadicList.unwrap());const compose = (f, g) => ((x) => f(g(x)));
-const addThree = (x) => x + 3;
-const addFive = (x) => x + 5;
-const addEight = compose(addThree, addFive);
-
-console.log("regular composition", addEight(5));
-
-// class Monad m where
-//   (>>=) :: m a -> (a -> m b) -> m b
-//   return :: a -> m a
-
-
-class Reader {
-  constructor(f) {
-    this.f = f;
-  }
-
-  bind(b) {
-    return new Reader(compose(b, this.f));
-  }
-
-  static return(a) {
-    return new Reader(() => a);
-  }
-
-  run() {
-    return this.f();
-  }
-}
-
-const monadic = Reader.return(5).bind(addThree).bind(addFive).run();
-console.log("monadic composition", monadic);
 
 class List {
   constructor(list) {
